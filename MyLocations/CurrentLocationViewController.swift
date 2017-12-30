5ee9a778c94c5114d82f6e743574b5b864f54e55
Kamil <amti7@o2.pl>
2017-12-30 22:34:33 +0100

    Adding location manager

--- MyLocations/CurrentLocationViewController.swift
+++ MyLocations/CurrentLocationViewController.swift
@@ -5,11 +5,12 @@
 //  Created by Kamil Gacek on 28/12/17.
 //  Copyright © 2017 Kamil Gacek. All rights reserved.
 //
-
+import CoreLocation
 import UIKit
 
-class CurrentLocationViewController: UIViewController{
+class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate {
 
+    let locationManager = CLLocationManager()
     
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var latitudeLabel: UILabel!
@@ -18,8 +19,24 @@ class CurrentLocationViewController: UIViewController{
     @IBOutlet weak var tagButton: UIButton!
     @IBOutlet weak var getButton: UIButton!
     
+
+    
     @IBAction func getLocation() {
-     
+        let authStatus = CLLocationManager.authorizationStatus()
+        
+        if authStatus == .notDetermined {
+            locationManager.requestWhenInUseAuthorization()
+            return
+        }
+        
+        if authStatus == .denied || authStatus == .restricted {
+            showLocationServicesDeniedAllert()
+            return
+        }
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        locationManager.startUpdatingLocation()
     }
     
     override func viewDidLoad() {
@@ -31,5 +48,31 @@ class CurrentLocationViewController: UIViewController{
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("didFailWithError: \(error)")
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let newLocation = locations.last!
+        print("didUpdateLocations: \(newLocation)")
+    }
+    
+    func showLocationServicesDeniedAllert() {
+        let alert = UIAlertController(title: "Location Services Disabled", message: "Please enable location services for this app in Settings.", preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        
+        alert.addAction(okAction)
+    
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
+
+    
+    
+    
+
 }
 
