674c697accee78540df0db4789f6dc2129aba8f2
Kamil <amti7@o2.pl>
2018-01-06 20:38:20 +0100

    Printing coordinates on the screen

--- MyLocations/CurrentLocationViewController.swift
+++ MyLocations/CurrentLocationViewController.swift
@@ -11,29 +11,25 @@ import UIKit
 class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate {
 
     let locationManager = CLLocationManager()
-    
+    var location: CLLocation?
+ 
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var latitudeLabel: UILabel!
     @IBOutlet weak var longitudeLabel: UILabel!
-    @IBOutlet weak var adressLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
     @IBOutlet weak var tagButton: UIButton!
     @IBOutlet weak var getButton: UIButton!
     
-
-    
     @IBAction func getLocation() {
         let authStatus = CLLocationManager.authorizationStatus()
-        
         if authStatus == .notDetermined {
             locationManager.requestWhenInUseAuthorization()
             return
         }
-        
         if authStatus == .denied || authStatus == .restricted {
             showLocationServicesDeniedAllert()
             return
         }
-        
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
         locationManager.startUpdatingLocation()
@@ -41,38 +37,47 @@ class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        updateLabels()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         print("didFailWithError: \(error)")
     }
     
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    func locationManager(_ manager: CLLocationManager,
+                         didUpdateLocations locations: [CLLocation]) {
         let newLocation = locations.last!
-        print("didUpdateLocations: \(newLocation)")
+        print("didUpdateLocations \(newLocation)")
+        location = newLocation
+        updateLabels()
     }
     
     func showLocationServicesDeniedAllert() {
         let alert = UIAlertController(title: "Location Services Disabled", message: "Please enable location services for this app in Settings.", preferredStyle: .alert)
         
         let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
-        
         alert.addAction(okAction)
-    
         present(alert, animated: true, completion: nil)
     }
     
-    
-
-    
-    
-    
-
+    func updateLabels() {
+        if let location = location {
+            latitudeLabel.text =
+                String(format: "%.8f", location.coordinate.latitude)
+            longitudeLabel.text =
+                String(format: "%.8f", location.coordinate.longitude)
+            tagButton.isHidden = false
+            messageLabel.text = ""
+        } else {
+            latitudeLabel.text = ""
+            longitudeLabel.text = ""
+            addressLabel.text = ""
+            tagButton.isHidden = true
+            messageLabel.text = "Tap 'Get My Location' to Start"
+        }
+    }
 }
-
