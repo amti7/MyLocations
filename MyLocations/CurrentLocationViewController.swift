3d873f656e7516b588051759bec251fe28dbcf9c
Kamil <amti7@o2.pl>
2018-01-24 18:03:09 +0100

    Creating CategoryPickerViewController

--- MyLocations/CurrentLocationViewController.swift
+++ MyLocations/CurrentLocationViewController.swift
@@ -232,4 +232,14 @@ class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate
             configureGetViewButton()
         }
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "TagLocation" {
+            let navigationController = segue.destination as! UINavigationController
+            let controller = navigationController.topViewController as! LocationDetailsViewController
+            
+            controller.coordinate = location!.coordinate
+            controller.placemark = placemark
+        }
+    }
 }
